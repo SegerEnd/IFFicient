684cc233a40314020a34ddcffdba75dfc2962537
Seger End <gj.endendijk@gmail.com>
2024-12-29 15:14:11 +0100

    Changes

--- IFFChunk.cs
+++ IFFChunk.cs
@@ -4,70 +4,35 @@ namespace IFFicient
 {
     public class IFFChunk
     {
-        public string Type { get; }
-        public byte[] Content { get; }
-        public List<IFFChunk> SubChunks { get; } = new List<IFFChunk>();
+        public string ChunkId { get; }
+        public uint Size { get; }
+        public byte[] Data { get; }
 
-        public IFFChunk(string type, byte[] content)
+        public IFFChunk(string chunkId, uint size, byte[] data)
         {
-            Type = type;
-            Content = content;
+            ChunkId = chunkId.PadRight(4, ' '); // Right-pad with spaces if less than 4 chars
+            Size = size;
+            Size = size;
+            Data = data;
         }
 
-        public int Size
+        public static IFFChunk ReadChunk(BinaryReader br)
         {
-            get
-            {
-                int size = Content.Length + 8; // Type + Size + Content
-                if (Content.Length % 2 != 0) size++; // Padding
-                size += SubChunks.Sum(sub => sub.Size); // Size of subchunks
-                return size;
-            }
+            var chunkId = new string(br.ReadChars(4));
+            uint size = br.ReadUInt32();
+            byte[] data = br.ReadBytes((int)size);
+            // Skip padding byte if size is odd
+            if (size % 2 != 0) br.ReadByte();
+
+            return new IFFChunk(chunkId, size, data);
         }
 
-        public void AddSubChunk(IFFChunk chunk)
-        {
-            SubChunks.Add(chunk);
-        }
-
-        // Write this chunk and all its subchunks recursively
         public void Write(BinaryWriter bw)
         {
-            bw.Write(Type.ToCharArray());
-            bw.Write(Size - 8); // Size of content + subchunks, excluding type and size fields
-            bw.Write(Content);
-            if (Content.Length % 2 != 0) bw.Write((byte)0); // Padding
-
-            foreach (var subChunk in SubChunks)
-            {
-                subChunk.Write(bw);
-            }
-        }
-
-        // Static method to read a chunk and its subchunks recursively
-        public static IFFChunk Read(BinaryReader br)
-        {
-            var type = IFFContainer.ReadString(br, 4);
-            var size = br.ReadInt32();
-            var content = br.ReadBytes(size);
-            var chunk = new IFFChunk(type, content);
-
-            // Handle padding
-            if (size % 2 != 0)
-            {
-                br.ReadByte(); // Skip padding byte
-            }
-
-            // Read subchunks if any remain in the current chunk's size
-            var remainingSize = size - content.Length;
-            while (remainingSize > 0)
-            {
-                var subChunk = IFFChunk.Read(br);
-                chunk.AddSubChunk(subChunk);
-                remainingSize -= subChunk.Size;
-            }
-
-            return chunk;
+            bw.Write(ChunkId.ToCharArray());
+            bw.Write(Size);
+            bw.Write(Data);
+            if (Data.Length % 2 != 0) bw.Write((byte)0); // Padding
         }
     }
 }
--- IFFFile.cs
+++ IFFFile.cs
@@ -7,14 +7,62 @@ using System.Threading.Tasks;
 
 namespace IFFicient
 {
-    public class IFFFile : IFFContainer
+    public class IFFFile
     {
-        protected override void WriteHeader(BinaryWriter bw)
+        private List<IFFChunk> Chunks { get; } = new List<IFFChunk>();
+
+        public void AddChunk(IFFChunk chunk)
         {
-            bw.Write("FORM".ToCharArray());
-            int totalSize = Chunks.Sum(c => c.Size);
-            bw.Write(totalSize); // Total size of all chunks
-            bw.Write("SIM ".ToCharArray()); // Format ID for The Sims 1
+            Chunks.Add(chunk);
+        }
+
+        public void WriteToFile(string filePath)
+        {
+            using var fs = new FileStream(filePath, FileMode.Create);
+            using var bw = new BinaryWriter(fs);
+
+            WriteIFFHeader(bw);
+            foreach (var chunk in Chunks)
+            {
+                chunk.Write(bw);
+            }
+        }
+
+        public static IFFFile ReadFromFile(string filePath)
+        {
+            var iff = new IFFFile();
+            using var fs = new FileStream(filePath, FileMode.Open);
+            using var br = new BinaryReader(fs);
+
+            var formId = new string(br.ReadChars(4));
+            if (formId != "DOLL") throw new FormatException("Not a correct DOLL / IFF file");
+
+            uint fileSize = br.ReadUInt32(); // Read and use the size 
+
+            // Log or use fileSize and typeId for validation or processing
+            //Console.WriteLine($"Reading DOLL file with size: {fileSize}");
+
+            long expectedEndPosition = br.BaseStream.Position + fileSize;
+            while (br.BaseStream.Position < expectedEndPosition)
+            {
+                iff.AddChunk(IFFChunk.ReadChunk(br));
+            }
+
+            // Check if we've read exactly the amount of data specified by the size field
+            //if (br.BaseStream.Position != expectedEndPosition)
+            //{
+            //    Console.WriteLine("Warning: File size does not match the expected size.");
+            //}
+
+            return iff;
+        }
+
+        private void WriteIFFHeader(BinaryWriter bw)
+        {
+            byte[] FORM = Encoding.ASCII.GetBytes("DOLL".PadRight(4, ' '));
+            bw.Write(FORM);
+            uint totalSize = (uint)Chunks.Sum(c => c.Size + 8 + (c.Size % 2 == 0 ? 0 : 1)); // Chunk size + header size + possible padding
+            bw.Write(totalSize);
         }
     }
 }
