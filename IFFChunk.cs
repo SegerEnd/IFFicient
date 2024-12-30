81b9b2d9059681d16dc6cde454dd298fbf4d5b09
Seger End <gj.endendijk@gmail.com>
2024-12-30 12:22:56 +0100

    Added comments and a few new functions

--- IFFChunk.cs
+++ IFFChunk.cs
@@ -4,18 +4,87 @@ namespace IFFicient
 {
     public class IFFChunk
     {
-        public string ChunkId { get; }
-        public uint Size { get; }
-        public byte[] Data { get; }
+        private string _chunkID = "ERR ";
 
+        /// <summary>
+        /// The ID of the chunk 4-character string (e.g. "NAME", "(c) ", "HOME", etc.)
+        /// The setter will right-pad the string with spaces if less than 4 characters & truncate if more than 4 characters
+        /// </summary>
+        public string ChunkId
+        {
+            get { return _chunkID; }
+            set { _chunkID = value.PadRight(4, ' ')[0..4]; }
+        }
+
+        public uint Size;
+
+        /// <summary>
+        /// The byte data of the chunk (e.g. the actual data of the chunk)
+        /// </summary>
+        public byte[] Data { get; set; }
+
+        /// <summary>
+        /// Creates a new IFFChunk object with the given chunk ID, size, and data
+        /// This is for full control over the chunk data, but it's my recommendation to use the other constructor without the size parameter
+        /// </summary>
+        /// <param name="chunkId">The 4-character string ID of the chunk (e.g. "NAME", "(c) ", "HOME", etc.)</param>
+        /// <param name="size">The size of only the data, not the size of the data + other bytes (e.g. chunk ID, size)</param>
+        /// <param name="data">Byte array of the data</param>
         public IFFChunk(string chunkId, uint size, byte[] data)
         {
-            ChunkId = chunkId.PadRight(4, ' '); // Right-pad with spaces if less than 4 chars
+            ChunkId = chunkId;
             Size = size;
             Size = size;
             Data = data;
         }
 
+        /// <summary>
+        /// Creates a new IFFChunk object with the given chunk ID and data
+        /// Automatically sets the size of the chunk based on the data length
+        /// </summary>
+        /// <param name="chunkId">The 4-character string ID of the chunk (e.g. "NAME", "(c) ", "HOME", etc.)</param>
+        /// <param name="data"></param>
+        public IFFChunk(string chunkId, byte[] data)
+        {
+            ChunkId = chunkId;
+            Size = (uint)data.Length;
+            Data = data;
+        }
+
+        /// <summary>
+        /// Creates a new empty IFFChunk object with the given chunk ID
+        /// </summary>
+        /// <param name="chunkId">The 4-character string ID of the chunk (e.g. "NAME", "(c) ", "HOME", etc.)</param>
+        public IFFChunk(string chunkId)
+        {
+            ChunkId = chunkId;
+            Data = new byte[0];
+            Size = (uint)Data.Length;
+        }
+
+        /// <summary>
+        /// Sets the given data to the chunk including its size.
+        /// </summary>
+        /// <param name="data">The string of data to set to the chunk</param>
+        public void AddData(string data)
+        {
+            Data = System.Text.Encoding.ASCII.GetBytes(data);
+            Size = (uint)Data.Length;
+        }
+
+        // Saves JSON to the data including size, where the data is not currently in byte[] format, Use a binary writer to write the data
+        //public void AddData(System.Text.Json.JsonDocument data)
+        //{
+        //    Data = System.Text.Encoding.ASCII.GetBytes(data.RootElement.GetRawText());
+        //    Size = (uint)Data.Length;
+        //}
+
+
+        /// <summary>
+        /// Creates and reads a chunk on the current position of the BinaryReader
+        /// </summary>
+        /// <param name="br">The BinaryReader to read the chunk from</param>
+        /// <returns>A new IFFChunk object containing the chunk data, ID, and size</returns>
         public static IFFChunk ReadChunk(BinaryReader br)
         {
             var chunkId = new string(br.ReadChars(4));
@@ -27,6 +96,10 @@ namespace IFFicient
             return new IFFChunk(chunkId, size, data);
         }
 
+        /// <summary>
+        /// Writes the chunk to the BinaryWriter at its current position
+        /// </summary>
+        /// <param name="bw">The BinaryWriter to write the chunk to</param>
         public void Write(BinaryWriter bw)
         {
             bw.Write(ChunkId.ToCharArray());
--- IFFFile.cs
+++ IFFFile.cs
@@ -9,25 +9,89 @@ namespace IFFicient
 {
     public class IFFFile
     {
+        /// <summary>
+        /// List of chunks in the IFF file
+        /// </summary>
         private List<IFFChunk> Chunks { get; } = new List<IFFChunk>();
 
+        /// <summary>
+        /// The FORM ID of the IFF file
+        /// </summary>
+        /// <remarks>
+        /// FORM is a form of chunks,
+        /// LIST is a list of chunks,
+        /// CAT is a catalog of chunks,
+        /// PROP is a property list
+        /// </remarks>
+        private string FormID = "FORM";
+
+        /// <summary>
+        /// List of valid FORM IDs
+        /// </summary>
+        private static readonly string[] FormIDs = { "FORM", "LIST", "CAT ", "PROP" };
+
+        /// <summary>
+        /// Adds a chunk to the IFF file chunks list
+        /// </summary>
+        /// <remarks>
+        /// All the chunks will be written to the file when calling WriteToFile
+        /// </remarks>
+        /// <param name="chunk"></param>
         public void AddChunk(IFFChunk chunk)
         {
             Chunks.Add(chunk);
         }
 
-        public void WriteToFile(string filePath)
+        /// <summary>
+        /// Gets a chunk by its chunk ID
+        /// </summary>
+        /// <remarks>
+        /// A chunk ID is a 4-character string, e.g. "NAME", "(c) ", "HOME", etc.
+        /// </remarks>
+        public IFFChunk GetChunk(string chunkId)
         {
-            using var fs = new FileStream(filePath, FileMode.Create);
-            using var bw = new BinaryWriter(fs);
+            return Chunks.FirstOrDefault(c => c.ChunkId == chunkId);
+        }
 
-            WriteIFFHeader(bw);
-            foreach (var chunk in Chunks)
+        //public IFFChunk GetChunk(int index)
+        //{
+        //    return Chunks[index];
+        //}
+
+        /// <summary>
+        /// Writes the IFF file to a file at the specified path
+        /// </summary>
+        /// <returns>True if the file was written successfully, false otherwise </returns>
+        public bool WriteToFile(string filePath)
+        {
+            try
             {
-                chunk.Write(bw);
+                using var fs = new FileStream(filePath, FileMode.Create);
+                using var bw = new BinaryWriter(fs);
+
+                WriteIFFHeader(bw, FormID);
+                foreach (var chunk in Chunks)
+                {
+                    chunk.Write(bw);
+                }
+
+                // free resources
+                bw.Close();
+                fs.Close();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
             }
         }
 
+        /// <summary>
+        /// Reads an IFF file from the specified path
+        /// </summary>
+        /// <param name="filePath">The path to the IFF file</param>
+        /// <returns>The complete IFF file object</returns>
         public static IFFFile ReadFromFile(string filePath)
         {
             var iff = new IFFFile();
@@ -35,14 +99,15 @@ namespace IFFicient
             using var br = new BinaryReader(fs);
 
             var formId = new string(br.ReadChars(4));
-            if (formId != "DOLL") throw new FormatException("Not a correct DOLL / IFF file");
+            //if (formId != FormID) throw new FormatException("Not a correct DOLL / IFF file");
+            if (!FormIDs.Contains(formId)) throw new FormatException("Not a correct DOLL / IFF file");
 
-            uint fileSize = br.ReadUInt32(); // Read and use the size 
+            ulong fileSize = br.ReadUInt64(); // Read and use the size 
 
             // Log or use fileSize and typeId for validation or processing
             //Console.WriteLine($"Reading DOLL file with size: {fileSize}");
 
-            long expectedEndPosition = br.BaseStream.Position + fileSize;
+            long expectedEndPosition = br.BaseStream.Position + (long)fileSize;
             while (br.BaseStream.Position < expectedEndPosition)
             {
                 iff.AddChunk(IFFChunk.ReadChunk(br));
@@ -57,12 +122,30 @@ namespace IFFicient
             return iff;
         }
 
-        private void WriteIFFHeader(BinaryWriter bw)
+        /// <summary>
+        /// Writes the IFF header to the file
+        /// Includes the FORM ID and the total size of the file
+        /// </summary>
+        /// <param name="bw"></param>
+        private void WriteIFFHeader(BinaryWriter bw, string FormID)
         {
-            byte[] FORM = Encoding.ASCII.GetBytes("DOLL".PadRight(4, ' '));
+            byte[] FORM = Encoding.ASCII.GetBytes(FormID.PadRight(4, ' '));
             bw.Write(FORM);
-            uint totalSize = (uint)Chunks.Sum(c => c.Size + 8 + (c.Size % 2 == 0 ? 0 : 1)); // Chunk size + header size + possible padding
+            ulong totalSize = (ulong)Chunks.Sum(c => c.Size + 8 + (c.Size % 2 == 0 ? 0 : 1)); // Total size calculation
             bw.Write(totalSize);
+        }
+
+        /// <summary>
+        /// Returns a string with the information of the IFF file
+        /// </summary>
+        public override string ToString()
+        {
+            // give a string representation of the IFF file with name, total size, amount of chunks and size per chunk
+            //string result = $"File: {FormID}\n";
+            //result += $"Total size: {Chunks.Sum(c => c.Size + 8 + (c.Size % 2 == 0 ? 0 : 1))}\n";
+            //result += $"Amount of chunks: {Chunks.Count}\n";
+            //result += $"Size per chunk: {Chunks.Average(c => c.Size)}\n";
+            return $"File: {FormID}, Total size: {Chunks.Sum(c => c.Size + 8 + (c.Size % 2 == 0 ? 0 : 1))}, Amount of chunks: {Chunks.Count}, Size per chunk: {Chunks.Average(c => c.Size)}";
         }
     }
 }
